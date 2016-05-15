40222ff5b7648989ae19671221406558282d480c
RishiSKeshav <rishiskeshav91@gmail.com>
2016-05-15 10:03:51 -0700

    gps changes

--- app/src/main/java/com/training1/myapplication/LocationClass.java
+++ app/src/main/java/com/training1/myapplication/LocationClass.java
@@ -9,13 +9,18 @@ import android.location.LocationManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
  * Created by RishiS on 5/12/2016.
  */
-public class LocationClass implements LocationListener {
 
+public class LocationClass implements LocationListener{
+
+    private final String PERMISSION_NOT_GRANTED="permission_not_granted";
+    private final String NO_PROVIDER="NO_PROVIDER";
+    private final String SOMETHING_WENT_WRONG="SOMETHING_WENT_WRONG";
     private double latitude;
     private double longitude;
 
@@ -24,11 +29,70 @@ public class LocationClass implements LocationListener {
         this.longitude=0;
     }
 
-    @Override
-    public void onLocationChanged(android.location.Location location) {
+    public String currentLocation(Context context){
 
-        this.latitude=location.getLatitude();
-        this.longitude=location.getLongitude();
+        LocationManager locationManager=null;
+        Location location=null;
+        String returnValue=null;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) context
+                    .getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        if (isGpsEnabled(locationManager)) {
+            returnValue=getLocation(location,locationManager,context);
+        }else
+            returnValue= NO_PROVIDER;
+
+        Log.d("LocationClass",returnValue);
+
+        return returnValue;
+    }
+
+    private String getLocation(Location location,LocationManager locationManager,Context context){
+
+        String returnValue=null;
+        // The minimum distance to change Updates in meters
+        final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 1 meters
+        // The minimum time between updates in milliseconds
+        final long MIN_TIME_BW_UPDATES = 15000;
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return PERMISSION_NOT_GRANTED;
+        }
+
+        if (location == null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+            if (locationManager != null) {
+                location = locationManager
+                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null) {
+                    returnValue= location.getLatitude()+","+location.getLongitude();
+                }
+                else{
+                    returnValue=SOMETHING_WENT_WRONG;
+                }
+            }
+        }else {
+            returnValue = location.getLatitude() + "," + location.getLongitude();
+        }
+
+        return returnValue;
+    }
+
+    private boolean isGpsEnabled(LocationManager locationManager){
+            return locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("LocationClass","onLocationChanged called");
+        latitude=location.getLatitude();
+        longitude=location.getLongitude();
     }
 
     @Override
@@ -44,34 +108,5 @@ public class LocationClass implements LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
 
-    }
-
-    public String currentLocation(Context context){
-
-        String mprovider;
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-
-        mprovider = locationManager.getBestProvider(criteria, false);
-
-        if (mprovider != null && !mprovider.equals("")) {
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                return "permission not granted";
-            }
-
-            Location location = locationManager.getLastKnownLocation(mprovider);
-            locationManager.requestLocationUpdates(mprovider, 15000, 1, this);
-
-            if (location != null) {
-                onLocationChanged(location);
-            }
-
-            else
-                Toast.makeText(context, "No Location Provider Found Check Your Code", Toast.LENGTH_SHORT).show();
-        }
-
-        return this.latitude+","+this.longitude;
     }
 }
--- app/src/main/java/com/training1/myapplication/LocationFragment.java
+++ app/src/main/java/com/training1/myapplication/LocationFragment.java
@@ -4,16 +4,20 @@ package com.training1.myapplication;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.location.Location;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.training1.myapplication.Model.City;
 
@@ -36,6 +40,9 @@ public class LocationFragment extends Fragment {
     private TextView windDirection;
     private TextView windSpeed;
     private City city;
+    private final String PERMISSION_NOT_GRANTED = "permission_not_granted";
+    private final String NO_PROVIDER = "NO_PROVIDER";
+    private final String SOMETHING_WENT_WRONG="SOMETHING_WENT_WRONG";
 
     public LocationFragment() {
         // Required empty public constructor
@@ -46,19 +53,19 @@ public class LocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_location,container,false);
+        view = inflater.inflate(R.layout.fragment_location, container, false);
 
         location = new LocationClass();
         receiver = new ResultBroadcastReceiver();
         resultFilter = new IntentFilter("locationCompleteAction");
 
-        cityName =(TextView) view.findViewById(R.id.city);
+        cityName = (TextView) view.findViewById(R.id.city);
         degree = (TextView) view.findViewById(R.id.degree);
-        weather =(TextView) view.findViewById(R.id.weather);
-        feelsLike =(TextView) view.findViewById(R.id.feelsLike);
-        humidity =(TextView) view.findViewById(R.id.humidity);
-        windDirection =(TextView) view.findViewById(R.id.windDirection);
-        windSpeed =(TextView) view.findViewById(R.id.windspeed);
+        weather = (TextView) view.findViewById(R.id.weather);
+        feelsLike = (TextView) view.findViewById(R.id.feelsLike);
+        humidity = (TextView) view.findViewById(R.id.humidity);
+        windDirection = (TextView) view.findViewById(R.id.windDirection);
+        windSpeed = (TextView) view.findViewById(R.id.windspeed);
 
         pd = new ProgressDialog(getActivity());
         pd.setMessage("Downloading please wait...");
@@ -71,8 +78,22 @@ public class LocationFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        getContext().registerReceiver(receiver,resultFilter);
-        new WeatherAsyncTaskLocation(getActivity(),pd).execute(location.currentLocation(getActivity()));
+        getContext().registerReceiver(receiver, resultFilter);
+
+        String flag = location.currentLocation(getActivity());
+        if (flag != null) {
+            if (flag.equals(PERMISSION_NOT_GRANTED)) {
+                Toast.makeText(getContext(), "Permission not granted", Toast.LENGTH_SHORT).show();
+            } else if (flag.equals(NO_PROVIDER)) {
+                showSettingsAlert();
+            }else if(flag.equals(SOMETHING_WENT_WRONG)) {
+                Toast.makeText(getContext(), "OOPS!! Something went wrong", Toast.LENGTH_SHORT).show();
+            }else{
+                new WeatherAsyncTaskLocation(getActivity(), pd).execute(location.currentLocation(getActivity()));
+            }
+        }
+        else
+            Log.d("flag","null");
     }
 
     @Override
@@ -82,24 +103,51 @@ public class LocationFragment extends Fragment {
         getContext().unregisterReceiver(receiver);
     }
 
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+
+        // Setting Dialog Title
+        alertDialog.setTitle("Location setting");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("Location is not allowed. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
     public class ResultBroadcastReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
 
             Log.d("locationFragment", "onRecieve");
-            if(pd.isShowing())
+            if (pd.isShowing())
                 pd.dismiss();
 
-            city =(City) intent.getExtras().get("cityByLocation");
+            city = (City) intent.getExtras().get("cityByLocation");
 
             cityName.setText(city.getCityName());
             degree.setText(city.getDegree());
             weather.setText(city.getWeather());
-            feelsLike.setText("Feels Like "+city.getFeelsLike());
-            humidity.setText("Humidity "+city.getHumidity());
-            windDirection.setText("Wind "+ city.getWindDirection());
-            windSpeed.setText(" "+ city.getWindSpeed());
+            feelsLike.setText(city.getFeelsLike());
+            humidity.setText(city.getHumidity());
+            windDirection.setText(city.getWindDirection());
+            windSpeed.setText(city.getWindSpeed());
         }
     }
 }
